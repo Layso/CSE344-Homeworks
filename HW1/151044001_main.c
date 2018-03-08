85a67e1027fa0439f1f3fc3c6e399400c64c85ee
Can <laysogame@hotmail.com>
2018-03-08 23:09:37 +0300

    File check implemented for given file name

--- HW1/151044001_main.c
+++ HW1/151044001_main.c
@@ -1,7 +1,108 @@
+/* Include(s) */
 #include "stdio.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "unistd.h"
+#include "errno.h"
+#include "string.h"
+
+
+
+/* Macro(s) to use as constant definition(s) */
+#define ZERO 0
+#define TRUE 1
+#define FALSE 0
+#define REQUIRED_ARGUMENT_COUNT 2
+#define EXE_NAME_INDEX 0
+#define FILE_NAME_INDEX 1
+#define ERROR_EXIT_CODE 1
+#define SAFE_EXIT_CODE 0
+#define ERROR_CODE -1
+#define EXTENSION_LENGTH 4
+#define TIF_EXTENSION ".tif"
+
+
+
+/* Function prototype(s) */
+int ScanAndPrint(char fileName[]);
+int CheckFile(char fileName[]);
+
 
 
 int main(int argc, char *argv[]) {
-    printf("heleleey");
-    return 0;
+    /* Checking argument count to print usage in case */
+    if (argc != REQUIRED_ARGUMENT_COUNT) {
+        printf("Usage: %s filename.tif\n", argv[EXE_NAME_INDEX]);
+    }
+    
+    else {
+        ScanAndPrint(argv[FILE_NAME_INDEX]);
+    }
+
+    return SAFE_EXIT_CODE;
+}
+
+
+
+/* Main function to scan given file and print to console */
+int ScanAndPrint(char fileName[]) {
+    /* Checking file first */
+    printf("Controlling file \"%s\"\n", fileName);
+    if (CheckFile(fileName) == ERROR_CODE) {
+        return ERROR_EXIT_CODE;
+    }
+
+
+
+    return SAFE_EXIT_CODE;
+}
+
+
+
+/* Function to check if file exists and safe to read */
+int CheckFile(char fileName[]) {
+    struct stat fileStats;
+    int file = ZERO;
+    int status = ZERO;
+    int nameLength = ZERO;
+    int i;
+
+
+    /* Checking file name if it's extension is .tif */
+    nameLength = strlen(fileName);
+    if (nameLength > EXTENSION_LENGTH) {
+        for (i=0; i<EXTENSION_LENGTH; ++i) {
+            if (fileName[nameLength-i-1] != TIF_EXTENSION[EXTENSION_LENGTH-i-1]) {
+                fprintf(stderr, "\nError!\nFile type is not supported: %s\nExpected extension: %s\n", fileName, TIF_EXTENSION);
+                return ERROR_CODE;
+            }
+        }
+    } else {
+        fprintf(stderr, "\nError!\nFile name is too short to be a TIFF file: %s\n", fileName);
+        return ERROR_CODE;
+    }
+    
+    /* Checking if path points to a file */
+    status = stat(fileName, &fileStats);
+    if (status == ERROR_CODE || !S_ISREG(fileStats.st_mode)) {
+        fprintf(stderr, "\nError!\nEither path is not a file or couldn't read path stats: %s\n", fileName);
+        return ERROR_CODE;
+    }
+
+    /* Checking if file can be opened */
+    file = open(fileName, O_RDONLY);
+    if (file == ERROR_CODE) {
+        fprintf(stderr, "\nError!\nCouldn't open file to check: %s\nReason of error: %s\n", fileName, strerror(errno));
+        return ERROR_CODE;
+    }
+
+    /* Closing file to release it */
+    status = close(file);
+    if (status == ERROR_CODE) {
+        fprintf(stderr, "\nError!\nCouldn't close file after check: %s\nReason of error: %s\n", fileName, strerror(errno));
+        return ERROR_CODE;
+    }
+
+
+    return SAFE_EXIT_CODE;
 }
