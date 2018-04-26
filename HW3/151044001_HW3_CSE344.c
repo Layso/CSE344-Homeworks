bfa13711fa1a51906a7a8ee38acd88036e34f563
Can <laysogame@hotmail.com>
2018-04-26 13:28:40 +0300

    Line reader implemented

--- HW3/151044001_HW3_CSE344.c
+++ HW3/151044001_HW3_CSE344.c
@@ -1,9 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "151044001_HW3_CSE344.h"
+
+
+
+
+
 
 
 int main(int argc, char *argv[]) {
+	char *lastCommand = NULL;
+	
+	
+	do {
+		lastCommand = ReadLine();
+		
+		printf("Read commmand: %s\n", lastCommand);
+		free(lastCommand);
+		lastCommand = NULL;	
+	} while(lastCommand != NULL);
 	
 	
 	return EXIT_SUCCESS;
+}
+
+
+
+/* Function to read input without having any string size */
+char *ReadLine() {
+	char *command = NULL;
+	int continueRead = TRUE;
+	int length = ZERO;
+	int bytesRead;
+	
+	
+	/* Allocationg initial space for command */
+	if ((command = malloc(CHAR_LENGTH_INTERVAL * sizeof(char))) == NULL) {
+		fprintf(stderr, "\nError\nMemory allocation to read input failed\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	else {
+		do {
+			/* Read from stdin to command variable */
+			bytesRead = read(STDIN_FILENO, command+length, CHAR_LENGTH_INTERVAL);length += bytesRead;
+			command = realloc(command, length+CHAR_LENGTH_INTERVAL);
+			
+			/* If new line character read, end the input taking process */
+			if (command[length-1] == CHAR_NEW_LINE) {
+				command[length-1] = CHAR_NULL;
+				continueRead = FALSE;
+			}
+		} while(continueRead);
+	}
+	
+	
+	return command;
 }
---
+++ HW3/151044001_HW3_CSE344.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+/* Constant definitions */
+#define ZERO 0
+#define TRUE 1
+#define FALSE 0
+#define ERROR_CODE -1
+#define CHAR_LENGTH_INTERVAL 10
+#define CHAR_NEW_LINE '\n'
+#define CHAR_NULL '\0'
+#define COMMAND_LS "ls"
+#define COMMAND_PWD "pwd"
+#define COMMAND_CD "cd"
+#define COMMAND_HELP "help"
+#define COMMAND_CAT "cat"
+#define COMMAND_WC "wc"
+#define COMMAND_EXIT "exit"
+
+
+
+/*  */
+char *ReadLine();
+#endif
