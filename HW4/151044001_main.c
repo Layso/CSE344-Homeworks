170f7b16595b0c373164f0cf883d98595b001036
Can <laysogame@hotmail.com>
2018-05-01 23:34:01 +0300

    Shared memory example implemented

--- HW4/151044001_main.c
+++ HW4/151044001_main.c
@@ -1,9 +1,56 @@
 #include "151044001_main.h"
 
 
+void parent(int fd) {
+	int *memory;
+	int i = ZERO;
+	
+	
+	printf("i is: %d\n", i);
+	memory = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, ZERO);
+	sleep(2);
+	i = *memory;
+	
+	printf("i is: %d\n", i);
+}
+
+void child(int fd) {
+	int *memory;
+	int i = 2;
+	
+	memory = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, ZERO);
+	printf("mem is: %d\n", *memory);
+	*memory = i;
+	printf("mem is: %d\n", *memory);
+	
+	_exit(EXIT_SUCCESS);
+}
+
 
 int main(int argc, char **argv) {
-	printf("heheyt\n");
+	int sharedDescriptor;
 	
+	
+	/* Creating descriptor with the desired shared memory name */
+	sharedDescriptor = shm_open(SHARED_MEMORY_NAME, O_CREAT | O_RDWR, FILE_PERMISSIONS);
+	if (sharedDescriptor == ERROR_CODE) {
+		fprintf(stderr, "\nSystem Error!\nShared memory creation failed\n%s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	
+	/* Changing size of file to the size of semaphores */
+	if (ftruncate(sharedDescriptor, sizeof(int)) == ERROR_CODE) {
+		fprintf(stderr, "\nSystem Error!\nMemory size extension failed\n%s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	
+	
+	switch(fork()) {
+		case 0:	child(sharedDescriptor); break;
+		default: parent(sharedDescriptor); wait(0); break;
+	}
+	
+	
+	shm_unlink(SHARED_MEMORY_NAME);
 	return EXIT_SUCCESS;
 }
--- HW4/151044001_main.h
+++ HW4/151044001_main.h
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <wait.h>
+#include <errno.h>
+
+
+#define ZERO 0
+#define ERROR_CODE -1
+#define FILE_PERMISSIONS S_IRUSR | S_IWUSR
+#define SHARED_MEMORY_NAME "/151044001shm"
