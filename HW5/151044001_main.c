ebbe58da9c09751994332ff8a54bc22038578342
Can <laysogame@hotmail.com>
2018-05-22 11:07:22 +0300

    Statistics of the program now printed at the end

--- HW5/151044001_main.c
+++ HW5/151044001_main.c
@@ -1,4 +1,5 @@
 #include "151044001_main.h"
+#include "queue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -9,9 +10,10 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <math.h>
 
 
-#include "queue.h"
+
 
 
 
@@ -24,56 +26,140 @@ int keepWorking = TRUE;
 
 int main(int argc, char **argv) {
 	int i;
+	int floristIndex;
 	int floristCount, clientCount;
+	void *returnValue = NULL;
 	pthread_t *threadArray = NULL;
 	struct Client *clients = NULL;
+	struct Statistic *stats = NULL;
 	struct Florist *florists = NULL;
-	int a;
 	
+	
+	/* Controlling argument count to exit in case no filename has provided */
 	if (argc != REQUIRED_ARGC) {
 		fprintf(stderr, "Usage: %s [fileName]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	
 	
+	/* Parsing file to get florists, clients and their counts */
 	ParseFile(argv[1], &florists, &clients, &floristCount, &clientCount);
+	
+	
+	/* Allocating memory for arrays */
+	stats = malloc(sizeof(struct Statistic) * floristCount);
 	mutexes = malloc(sizeof(pthread_mutex_t) * floristCount);
 	workQueue = malloc(sizeof(Queue*) * floristCount);
 	threadArray = malloc(sizeof(pthread_t) * floristCount);
 	conditionVariables = malloc(sizeof(pthread_cond_t) * floristCount);
+	if (conditionVariables == NULL || threadArray == NULL || workQueue == NULL || florists == NULL || clients == NULL || mutexes == NULL || stats == NULL) {
+		fprintf(stderr, "\nSystem Error\nNot all memory allocations succesfully completed\nError message: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	
+	/* Creating thread pool before processing clients */
 	for (i=0; i<floristCount; ++i) {
 		workQueue[i] = NULL;
 		QueueInitialize(workQueue+i);
-		pthread_mutex_init(mutexes+i, NULL);
-		pthread_cond_init(conditionVariables+i, NULL);
-		pthread_create(threadArray+i, NULL, FloristThread, florists+i);
+		
+		if (pthread_mutex_init(mutexes+i, NULL) == ERROR_CODE) {
+			fprintf(stderr, "\nSystem Error\nMutex initializiation failed\nError message: %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
+		
+		if (pthread_cond_init(conditionVariables+i, NULL) == ERROR_CODE) {
+			fprintf(stderr, "\nSystem Error\nCondition variable initializiation failed\nError message: %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
+		
+		if (pthread_create(threadArray+i, NULL, FloristThread, florists+i) == ERROR_CODE) {
+			fprintf(stderr, "\nSystem Error\nThread creation failed\nError message: %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
+	}
+	printf("%d florists have been created\n", floristCount);
+	
+	
+	/* After everything is done, for each client get closest florist to client and redirect client to proper florist*/
+	for (i=0; i<clientCount; ++i) {
+		/* Get proper florist index */
+		floristIndex = GetProperFloristForClient(clients[i], florists, floristCount);
+		
+		/* If a florist has found with requested flower */
+		if (floristIndex >= ZERO) {
+			if (pthread_mutex_lock(&mutexes[floristIndex]) == ERROR_CODE) {
+				fprintf(stderr, "\nSystem Error\nMutex lock failed\nError message: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+			
+			QueueOffer(workQueue[floristIndex], clients[i]);
+			
+			if (pthread_cond_signal(&conditionVariables[floristIndex]) == ERROR_CODE) {
+				fprintf(stderr, "\nSystem Error\nCondition variable signal failed\nError message: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+			
+			if (pthread_mutex_unlock(&mutexes[floristIndex]) == ERROR_CODE) {
+				fprintf(stderr, "\nSystem Error\nMutex unlock failed\nError message: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+		}
+		
+		/* Else print proper message that there is no seller for required flower */
+		else {
+			fprintf(stderr, "No available florist found for %s's '%s' request\n", clients[i].name, clients[i].request);
+		}
 	}
 	
-	for (i=0; i<clientCount; ++i) {
-		/* TODO: Get the proper florist index to a variable with more meaningful name */
-		a = rand()%3;
-		
-		pthread_mutex_lock(&mutexes[a]);
-		QueueOffer(workQueue[a], clients[i]);
-		pthread_cond_signal(&conditionVariables[a]);
-		pthread_mutex_unlock(&mutexes[a]);
+	
+	
+	/* Acquire locks to notify all clients are proceeded */
+	for (i=0; i<floristCount; ++i) {
+		pthread_mutex_lock(&mutexes[i]);
 	}
 	
 	keepWorking = FALSE;
+	fprintf(stderr, "All requests are proceeded\n");	
+	
+	/* Release the locks to let them continue work */
 	for (i=0; i<floristCount; ++i) {
-		pthread_cond_signal(&conditionVariables[i]);
-		pthread_join(*(threadArray+i), NULL);
-		QueueDestruct(workQueue[i]);
+		pthread_mutex_unlock(&mutexes[i]);	
 	}
 	
+	
+	/* Wait for threads to complete their work, get florist stats and destroy florist specific allocations */
+	for (i=0; i<floristCount; ++i) {
+		pthread_cond_signal(&conditionVariables[i]);
+		pthread_join(*(threadArray+i), &returnValue);
+		stats[i] = *(struct Statistic*)returnValue;
+		free(returnValue);
+		QueueDestruct(workQueue[i]);
+		pthread_mutex_destroy(&mutexes[i]);
+		pthread_cond_destroy(&conditionVariables[i]);
+	}
+	
+	
+	/* Printing statistic of florists */
+	printf("\nSale statistics for today:\n");
+	printf("----------------------------------------------------------\n");
+	printf("Florist\t\t\t# of sales\t\tTotal time\n");
+	printf("----------------------------------------------------------\n");
+	for (i=0; i<floristCount; ++i) {
+		printf("%s\t\t\t%d\t\t\t%d\n", stats[i].name, stats[i].totalSales, stats[i].totalTime);
+	}
+	printf("----------------------------------------------------------\n");
+	
+	
+	/* Releasing the allocated memory spaces */
 	free(conditionVariables);
 	free(threadArray);
 	free(workQueue);
 	free(florists);
 	free(clients);
 	free(mutexes);
+	free(stats);
 	
-	
+	/* Finally exit program with success code */
 	return EXIT_SUCCESS;
 }
 
@@ -82,13 +168,15 @@ int main(int argc, char **argv) {
 void *FloristThread(void *parameter) {
 	struct Florist *info = (struct Florist*)parameter;
 	struct Client currentClient;
-	struct Statistic stats;
+	struct Statistic *stats;
 	int status;
+	int random;
 	
 	
-	strcpy(stats.name, info->name);
-	stats.totalSales = ZERO;
-	stats.totalTime = ZERO;
+	stats = malloc(sizeof(struct Statistic));
+	strcpy(stats->name, info->name);
+	stats->totalSales = ZERO;
+	stats->totalTime = ZERO;
 	
 	
 	while (!QueueEmpty(workQueue[info->id]) || keepWorking) {
@@ -101,12 +189,59 @@ void *FloristThread(void *parameter) {
 		
 		
 		if (status) {
-			printf("%s serving to %s for %s\n", info->name, currentClient.name, currentClient.request);
+			random = (rand() % RANDOM_INTERVAL) + RANDOM_BASE + DELIVERY_CONSTANT*(int)Distance(info->x, info->y, currentClient.x, currentClient.y);
+			/*sleep((int)(info->tick * 2));*/
+			printf("Florist %s has delivered %s to %s in %dms\n", info->name, currentClient.request, currentClient.name, random);
+			++(stats->totalSales);
+			(stats->totalTime) += random;
 		}
 	}
 
 	printf("Florist %s closing the shop\n", info->name);
-	return NULL;
+	return stats;
+}
+
+
+
+int GetProperFloristForClient(struct Client client, struct Florist *florists, int floristCount) {
+	int i;
+	int index = -1;
+	float distance = __DBL_MAX__;
+	float currentDistance = distance;
+	int x = client.x;
+	int y = client.y;
+	
+	
+	for (i=0; i<floristCount; ++i) {
+		currentDistance = Distance(florists[i].x, florists[i].y, x, y);
+		if (currentDistance < distance && GotFlowerOfType(client.request, florists[i].flowers, florists[i].flowerCount)) {
+			distance = currentDistance;
+			index = i;
+		}
+	}
+	
+	
+	return index;
+}
+
+
+
+int GotFlowerOfType(char *required, char saleList[ARRAY_LENGTH][STRING_LENGTH], int flowerCount) {
+	int i;
+	
+	for (i=0; i<flowerCount; ++i) {
+		if (!strcmp(required, saleList[i])) {
+			return TRUE;
+		}
+	}
+	
+	return FALSE;
+}
+
+
+
+float Distance(int x1, int y1, int x2, int y2) {
+	return sqrt(pow(x2 - x1, SQUARE) + pow(y2 - y1, SQUARE));
 }
 
 
--- HW5/151044001_main.h
+++ HW5/151044001_main.h
@@ -4,6 +4,10 @@
 #define ZERO 0
 #define TRUE 1
 #define FALSE 0
+#define SQUARE 2
+#define MICRO_TO_MILI 1000
+#define RANDOM_INTERVAL 40
+#define RANDOM_BASE 10
 #define ERROR_CODE -1
 #define REQUIRED_ARGC 2
 #define LINE_LENGTH 1024
@@ -11,6 +15,7 @@
 #define EMPTY_STRING "\n"
 #define READ_MODE "r"
 
+#define DELIVERY_CONSTANT 5
 #define STRING_LENGTH 64
 #define ARRAY_LENGTH 50
 
@@ -42,5 +47,7 @@ struct Statistic {
 
 void ParseFile(char *fileName, struct Florist **florists, struct Client **clients, int *floristCount, int *clientCount);
 void *FloristThread(void *parameter);
-
+int GetProperFloristForClient(struct Client client, struct Florist *florists, int floristCount);
+int GotFlowerOfType(char *required, char saleList[ARRAY_LENGTH][STRING_LENGTH], int flowerCount);
+float Distance(int x1, int y1, int x2, int y2);
 #endif
